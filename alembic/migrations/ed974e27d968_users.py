3840d36e1c05af1fff292298dc0a6e712a9b3589
Vadim Sokoltsov <vforvad@gmail.com>
2019-04-28 00:46:21 +0300

    Add alembic support

---
+++ alembic/migrations/ed974e27d968_users.py
@@ -0,0 +1,34 @@
+"""users
+
+Revision ID: ed974e27d968
+Revises: 
+Create Date: 2019-04-27 21:40:16.750514
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ed974e27d968'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
+        sa.Column('email', sa.String(255), nullable=False),
+        sa.Column('password', sa.String(255), nullable=False),
+        sa.Column('first_name', sa.String(255), nullable=True),
+        sa.Column('last_name', sa.String(255), nullable=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+    )
+    op.create_unique_constraint("unique_user_email", "users", ["email"])
+
+
+def downgrade():
+    op.drop_constraint('unique_user_email', 'users')
+    op.drop_table('users')
