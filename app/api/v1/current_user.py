abd5a86d6484878229f08960ece4799b1c2d3475
Vadim Sokoltsov <vforvad@gmail.com>
2019-07-19 20:56:32 +0300

    Add current_user route

---
+++ app/api/v1/current_user.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+from flask import request, g
+
+from app.services.authorization import authenticate
+from app.schemas.current_user import current_user_schema
+
+class CurrentUserResource(Resource):
+    """ Current User endpoint. """
+
+    method_decorators = [authenticate]
+
+    def get(self):
+        return {'current_user': current_user_schema.dump(g.user) }, 200
