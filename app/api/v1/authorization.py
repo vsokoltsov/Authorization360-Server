ad1c64e0ddf7ed71f148b5ef1a9bcca75551ebdb
Vadim Sokoltsov <vforvad@gmail.com>
2019-07-19 20:23:18 +0300

    Add tests for authorization

--- app/api/v1/authorization.py
+++ app/api/v1/authorization.py
@@ -1,9 +1,15 @@
-from . import Resource
+from . import Resource, request
 
+from app.forms.authorization import AuthorizationForm
 
 class AuthorizationResource(Resource):
     """ Authorization Resource class """
 
     def post(self):
         """ Authorize user via credentials """
-        return "Hello world"
+        
+        form = AuthorizationForm(params=request.get_json())
+        if form.submit():
+            return {'token': str(form.token)}, 200
+        else:
+            return {'errors': form.errors}, 400
